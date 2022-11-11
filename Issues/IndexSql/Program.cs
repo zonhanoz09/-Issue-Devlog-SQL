4b928941c1f56be95f95f3de5b579450380ef2bd
LAPTOP-AR72J0HF\ACER <huynhvannhancntt@gmail.com>
2022-11-11 16:22:05 +0700

    Index

---
+++ Issues/IndexSql/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var context = new TestContext();
+
+        for (int i = 0; i < 1000000; i++)
+        {
+            context.Todo.Add(new Todo
+            {
+                Name = $"TodoName{i}",
+                Comment = $"Commment{i}",
+                Content = $"Content{i}",
+                Count = i,
+            });
+        }
+
+        context.SaveChanges();
+    }
+}
+
---
+++ Issues/IndexSql/TestContext.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+public class TestContext : DbContext
+{
+    public virtual DbSet<Todo> Todo { get; set; }
+    public virtual DbSet<Answer> Answer { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQL2019;Database=TestDB;Trusted_Connection=True;");
+    }
+}
+
+[Index(nameof(Name), Name = "Nidx_Todo_Name")]
+[Index(nameof(Comment), Name = "Nidx_Todo_Comment")]
+//[Index(nameof(Name), nameof(Comment), Name = "Nidx_Todo_NameComment")]
+public class Todo
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+    public string Comment { get; set; }
+    public string Content { get; set; }
+    public int Count { get; set; }
+
+    public virtual ICollection<Answer> Answer { get; set; }
+}
+
+public class Answer
+{
+    [Key]
+    public int Id { get; set; }
+
+    public virtual Todo Todo { get; set; }
+}
