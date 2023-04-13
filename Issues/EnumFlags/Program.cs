f620ab4b57856d885aaac074a52ae1210ff06277
LAPTOP-AR72J0HF\ACER <huynhvannhancntt@gmail.com>
2023-04-13 10:59:23 +0700

    enum flag

---
+++ Issues/EnumFlags/Program.cs
@@ -0,0 +1,43 @@
+﻿//Check if the Value Exists
+if (Options.OneAndTwo.HasFlag(Options.One))
+{
+    Console.WriteLine("One");
+}
+if ((Options.OneAndTwo & Options.Three) == Options.One)
+{
+    Console.WriteLine("One");
+}
+
+//Add a Value 
+Options Addflags = Options.OneAndTwo;
+Addflags |= Options.Three;
+if (Addflags.HasFlag(Options.Three))
+{
+    Console.WriteLine("Three");
+}
+
+//Remove a Value
+Options Removeflags = Options.OneAndTwo;
+Removeflags &= ~Options.Two;
+if (Removeflags.HasFlag(Options.Two))
+{
+    Console.WriteLine("Two");
+}
+
+Console.WriteLine("Hello, World!");
+
+
+
+[Flags]
+public enum Options : byte
+{
+    None = 0,
+    One = 1 << 0,   // 1
+    Two = 1 << 1,   // 2
+    Three = 1 << 2,   // 4
+    Four = 1 << 3,   // 8
+
+    // combinations
+    OneAndTwo = One | Two,
+    OneTwoAndThree = One | Two | Three,
+}
---
+++ Issues/IndexSql/Migrations/20221111093626_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IndexSql.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Count = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TodoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answer", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answer_Todo_TodoId",
+                        column: x => x.TodoId,
+                        principalTable: "Todo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answer_TodoId",
+                table: "Answer",
+                column: "TodoId");
+
+            migrationBuilder.CreateIndex(
+                name: "Nidx_Todo_Comment",
+                table: "Todo",
+                column: "Comment");
+
+            migrationBuilder.CreateIndex(
+                name: "Nidx_Todo_Name",
+                table: "Todo",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answer");
+
+            migrationBuilder.DropTable(
+                name: "Todo");
+        }
+    }
+}
